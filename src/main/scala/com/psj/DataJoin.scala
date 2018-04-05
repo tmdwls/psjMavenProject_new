4cf8021d0732ea081f1d3808d787eed92bf49ae6
hyokwan <haiteam@kopo.ac.kr>
2018-04-05 14:58:20 +0900

    test example value added

--- src/main/scala/com/psj/DataJoin.scala
+++ src/main/scala/com/psj/DataJoin.scala
@@ -1,5 +1,33 @@
+
 package com.psj
 
-object DataJoin {
+import org.apache.spark.sql.SparkSession;
+
+object Example_Join {
+
+  val spark = SparkSession.builder().appName("...").
+    config("spark.master", "local").
+    getOrCreate()
+
+
+  var dataPath = "c:/spark/bin/data/"
+  var mainData = "kopo_channel_seasonality_ex.csv"
+  var subData = "kopo_product_mst.csv"
+
+  // relative path
+  var mainDataDf = spark.read.format("csv").option("header", "true").load(dataPath + mainData)
+
+  var subDataDf = spark.read.format("csv").option("header", "true").load(dataPath + subData)
+
+
+
+  mainDataDf.createOrReplaceTempView("mainTable")
+  subDataDf.createOrReplaceTempView("subTable")
+
+  var leftJoinData = spark.sql("select a.*, b.productname " +
+    "from maindata a left outer join subdata b " +
+    "on a.productgroup = b.productid")
+
+
 
 }
