bc204b3fdcd1c75214dadbb68361261d544d6ae6
tmdwls <tmdwls0831@naver.com>
2018-04-06 10:54:17 +0900

    test example value added

--- src/main/scala/com/psj/Example_DataLoding.scala
+++ src/main/scala/com/psj/Example_DataLoding.scala
@@ -53,6 +53,27 @@ object Example_DataLoding {
 
 
 
+    //데이터 파일 로딩
+    var dataPath = "./data/"
+    var selloutFile = "kopo_channel_seasonality.csv"
+
+    // relative path
+    //var selloutData1 = spark.read.format("csv").option("header", "true").load(dataPath + selloutFile)
+
+    // absolute path
+    var selloutData2 = spark.read.format("csv").option("header", "true").load("c:/spark/bin/data/" + selloutFile)
+
+    selloutData2.createOrReplaceTempView("selloutTable")
+    println(selloutData2.show)
+
+    ///////////////////////////     데이터 파일 로딩 ////////////////////////////////////
+    // 파일설정
+    dataPath = "./data/"
+    var paramFile = "KOPO_BATCH_SEASON_MPARA.txt"
+
+    // 절대경로 입력
+    var paramData= spark.read.format("csv").option("header","true").option("Delimiter",";").load("c:/spark/bin/data/"+paramFile)
+
 
 
   }
