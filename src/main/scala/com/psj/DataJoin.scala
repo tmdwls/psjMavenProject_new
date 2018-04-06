dd3c8b0fe7b0aa05782f77c33e06ecd764539ae0
tmdwls <tmdwls0831@naver.com>
2018-04-06 10:25:39 +0900

    test example value added

--- src/main/scala/com/psj/DataJoin.scala
+++ src/main/scala/com/psj/DataJoin.scala
@@ -35,4 +35,26 @@ object DataJoin {
   var leftJoinData = spark.sql("select a.*, b.productname " +
     "from maindata a left outer join subdata b " +
     "on a.productgroup = b.productid")
+
+
+//토드조인
+//  SELECT A.REGIONID, B.REGIONNAME, A.PRODUCT, A.YEARWEEK, A.QTY
+ // FROM KOPO_CHANNEL_SEASONALITY_NEW A
+ // LEFT JOIN KOPO_REGION_MST B
+ //   ON A.REGIONID = B.REGIONID
+
+//토드 3테이블 조인
+// select A.*
+// ,B.productname
+// ,c.promotion_ratio
+// ,d.promotion_Data
+// FROM lefttable A
+// LEFT JOIN righttable B
+//  ON A.productgroup = B.productid
+// LEFT JOIN table2 C
+//   ON A.productgroup = c.productgroup
+// AND A.yearweek = C.yearweek
+// LEFT JOIN table3 D
+//  ON A.productgroup = D.productgroup
+
 }
