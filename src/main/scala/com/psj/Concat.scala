ae2c9f3fb4dcc8f80fc2cdfcd391df1ce48b8e44
tmdwls <tmdwls0831@naver.com>
2018-04-12 17:07:48 +0900

    test example value added

--- src/main/scala/com/psj/Concat.scala
+++ src/main/scala/com/psj/Concat.scala
@@ -2,10 +2,11 @@ package com.psj
 
 import org.apache.spark.sql.SparkSession
 
+
 object Concat {
 
   object test_Loding {
-    val spark = SparkSession.builder().appName("hkProject").
+    val spark = SparkSession.builder().appName("Concat").
       config("spark.master", "local").
       getOrCreate()
 
@@ -21,7 +22,18 @@ var middleResult = spark.sql("select " +
     "left join selloutTable1 b "+
     "on A.PRODUCT = B.PRODUCTID")
 
+///컬럼 인덱스 생성
 
+    var rawDataColumns = middleResult.columns
+
+    var keycolNO = rawDataColumns.indexOf("keycol")
+    var REGIONIDNO = rawDataColumns.indexOf("REGIONID")
+    var PRODUCTNO = rawDataColumns.indexOf("PRODUCT")
+    var YEARWEEKNO = rawDataColumns.indexOf("YEARWEEK")
+    var QTYNO = rawDataColumns.indexOf("QTY")
+    var PRODUCTNAMENO = rawDataColumns.indexOf("PRODUCTNAME")
+
+    var rawRdd = middleResult.rdd
 
 
 
