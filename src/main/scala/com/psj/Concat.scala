82d24b4e6883c8d184f9599928ef31a6983e9bcc
tmdwls <tmdwls0831@naver.com>
2018-04-13 10:22:12 +0900

    test example value added

--- src/main/scala/com/psj/Concat.scala
+++ src/main/scala/com/psj/Concat.scala
@@ -10,19 +10,43 @@ object Concat {
       config("spark.master", "local").
       getOrCreate()
 
+    //Oracle XE 데이터 불러오기
+    // 접속정보 설정//
+    var staticUrl = "jdbc:oracle:thin:@192.168.110.111:1521/orcl"
+    var staticUser = "kopo"
+    var staticPw = "kopo"
+    var selloutDb = "kopo_channel_seasonality_new"
+    var selloutDb1 ="kopo_product_master"
+
+    // jdbc (java database connectivity) 연결
+    val selloutDataFromOracle= spark.read.format("jdbc").
+      options(Map("url" -> staticUrl,"dbtable" -> selloutDb,"user" -> staticUser, "password" -> staticPw)).load
+    val selloutDataFromOracle1= spark.read.format("jdbc").
+      options(Map("url" -> staticUrl,"dbtable" -> selloutDb1,"user" -> staticUser, "password" -> staticPw)).load
+
+    // 메모리 테이블 생성
+    selloutDataFromOracle.createOrReplaceTempView("selloutTable")
+    selloutDataFromOracle.show(1)
+
+    selloutDataFromOracle1.createOrReplaceTempView("selloutTable1")
+    selloutDataFromOracle1.show(1)
+
+
+    var mainData = selloutDataFromOracle.rdd
+    var subData = selloutDataFromOracle1.rdd
 
 var middleResult = spark.sql("select " +
-    "concat(A.REGIONID,'_',A.PRODUCT) as keycol, " +
-    "A.REGIONID, " +
-    "A.PRODUCT, " +
-    "A.YEARWEEK, " +
-    "cast(A.QTY as double) as QTY, " +
-    "B.PRODUCTNAME " +
-    "from selloutTable a " +
-    "left join selloutTable1 b "+
-    "on A.PRODUCT = B.PRODUCTID")
+      "concat(A.REGIONID,'_',A.PRODUCT) as keycol, " +
+      "A.REGIONID, " +
+      "A.PRODUCT, " +
+      "A.YEARWEEK, " +
+      "cast(A.QTY as double) as QTY, " +
+      "B.PRODUCTNAME " +
+      "from selloutTable a " +
+      "left join selloutTable1 b "+
+      "on A.PRODUCT = B.PRODUCTID")
 
-///컬럼 인덱스 생성
+    ///컬럼 인덱스 생성
 
     var rawDataColumns = middleResult.columns
 
@@ -34,6 +58,8 @@ var middleResult = spark.sql("select " +
     var PRODUCTNAMENO = rawDataColumns.indexOf("PRODUCTNAME")
 
     var rawRdd = middleResult.rdd
+
+
 
 
 
