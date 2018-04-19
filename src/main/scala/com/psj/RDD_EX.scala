4ca462aabaeb6043384ea6b9c00c587045fa106b
tmdwls <tmdwls0831@naver.com>
2018-04-19 15:51:10 +0900

    test example value added

--- src/main/scala/com/psj/RDD_EX.scala
+++ src/main/scala/com/psj/RDD_EX.scala
@@ -2,6 +2,8 @@ package com.psj
 
 import com.psj.Concat.test_Loding.{middleResult, spark}
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.types.{StringType, DoubleType, StructField, StructType}//스파크에서도 실행
+
 
 object RDD_EX {
 
@@ -35,7 +37,7 @@ object RDD_EX {
 
 ////조인
   var middleResult = spark.sql("select " +
-    "concat(A.REGIONID,'_',A.PRODUCT) as keycol, " +
+   // "concat(A.REGIONID,'_',A.PRODUCT) as keycol, " +
     "A.REGIONID, " +
     "A.PRODUCT, " +
     "A.YEARWEEK, " +
@@ -48,7 +50,7 @@ object RDD_EX {
   //인텍스 부여
   var rawDataColumns = middleResult.columns
 
-  var keycolNO = rawDataColumns.indexOf("keycol")
+  //var keycolNO = rawDataColumns.indexOf("keycol")
   var REGIONIDNO = rawDataColumns.indexOf("REGIONID")
   var PRODUCTNO = rawDataColumns.indexOf("PRODUCT")
   var YEARWEEKNO = rawDataColumns.indexOf("YEARWEEK")
@@ -56,9 +58,6 @@ object RDD_EX {
   var PRODUCTNAMENO = rawDataColumns.indexOf("PRODUCTNAME")
 
   var rawRdd = middleResult.rdd
-
-
-
 
   //////////////////////////////////////////////////////////////////////
   // RDD-정제연산
@@ -70,25 +69,64 @@ object RDD_EX {
   //var {RDD변수명}.collect.foreach(println)
 
   var filterexRdd = rawRdd.filter(x=> {
-    // 데이터 한줄씩 들어옴
+    // Boolean  = true
     var checkValid = true
-    // 그중 특정 컬럼값
-    var yearweek = x.getString(YEARWEEKNO)
+   //찾기 : yearweek 인덱스로 주차정보만 인트타입으로 변환
+    var weekValue = x.getString(YEARWEEKNO).substring(4).toInt
 
-    if(yearweek.length != 6){checkValid = false}
+    //비교한후 주차정보가 53 인상인 경우 레코드 삭제
+    if(weekValue >= 53){checkValid = false}
     checkValid
   })
 
   var rwaExRdd = rawRdd.filter(x=>{
-    var checkValid = false
+    var checkValid = true
     if((x.getString(REGIONIDNO) == "A60") &&
       (x.getString(PRODUCTNO) == "PRODUCT34") &&
       (x.getString(YEARWEEKNO) == "201402")){
-      checkValid = true
+      checkValid = false
     }
     checkValid
   })
+  var x = resultRdd.first
 
+//////////////////////////////////////////////////////////////////////////////////////////
+  var productArray = Array("PRODUCT1", "PRODUCT2")
+
+  var productSet = productArray.toSet
+
+  var resultRdd = filterexRdd.filter(x=> {
+    var checkValid = false
+
+    var productInfo = x.getString(PRODUCTNO);
+
+   if(productSet.contains(productInfo)){//contains --> 포함의 여부 확인 메서드
+     checkValid = true
+   }
+    checkValid
+  })
+
+  // 데이터 확인
+  //var {resultRdd}.collect.foreach(println)
+  resultRdd.collect.foreach(println)
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//RDD → Dataframe
+  //var {DataFrame 변수명} = spark.createDataframe( {RDD명},
+ //   StructType(Seq( StructField( “컬럼명#1”, 데이터타입#1),
+ // StructField( “컬럼명#2”, 데이터타입#2))
+  resultRdd
+
+var productDataFrame = spark.createDataFrame(resultRdd,
+  StructType(
+    Seq(
+      StructField("KEY", StringType),
+      StructField("REGIONID", StringType),
+      StructField("PRODUCT", StringType),
+      StructField("YEARWEEK", StringType),
+      StructField("VOLUME", DoubleType),
+      StructField("PRODUCT_NAME", StringType))))
 
 
 
